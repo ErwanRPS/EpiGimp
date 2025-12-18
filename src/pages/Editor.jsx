ee20b45abcdd4b5293febb228aec17d577610dfa
ErwanRPS <erwan.ropars@epitech.eu>
2025-12-18 19:06:52 +0100

    feat: add Editor page component

---
+++ src/pages/Editor.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import Workspace from '../components/Workspace';
+
+function Editor() {
+  return (
+    <Layout>
+      <Workspace />
+    </Layout>
+  );
+}
+
+export default Editor;
